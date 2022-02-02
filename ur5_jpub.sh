e7173b8ae2366efcbdfe88a8b27c4907cf767989
Shubham Sonawani <sdsonawa@asu.edu>
2022-02-01 18:08:34 -0700

    parameter information

---
+++ ur5_jpub.sh
@@ -0,0 +1,10 @@
+rostopic pub /ur5/control irl_robots/ur5Control "command: 'movej'
+values: [0, -1.57, 0.0, 1.57, 0.0, 0.0]
+acceleration: 1.57
+velocity: 1.57
+time: 1.0
+blend: 0.0
+gain: 0.0
+lookahead: 0.0
+jointcontrol: true" 
+
